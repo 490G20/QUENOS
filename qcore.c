92bd4269bc7bbb1cee2769a51e9205e0a9fbb131
gs <9152942+usernameisntallowed@users.noreply.github.com>
2017-11-27 22:26:45 -0800

    Separated exception handler code into it's own file to better mirror lab7 from 371, (it's lab 4 in the 2017 iteration of the course), updated nios2_ctrl_reg_macros.h similarly

--- qcore.c
+++ qcore.c
@@ -121,6 +121,7 @@ static  int     QuenosCoreUnblock (int other_pid)
    To make sure that we retain control over the stack pointer, the
    variables are made static outside the functions. */
 
+//TODO: remove and refactor as we don't want to asm directives
 /**
 	Expect kernel_stack_pointer_address to magically get into register 4, which we will then write into the stackpoiter
 */
@@ -131,127 +132,30 @@ void writeRegisterValueToSP (void* registerValue){
 static  int     need_dispatch;
 static  Request request;
 
-void QuenosSaveContext (void) {
-    asm(".set noat"); // magic, for the C compiler
-    asm(".set nobreak"); // magic, for the C compiler
-    asm("subi sp, sp, 128");
-    asm("stw et, 96(sp)");
-    asm("rdctl et, ctl4");
-    asm("beq et, r0, SKIP_EA_DEC"); // interrupt is not external
-    asm("subi ea, ea, 4"); /* must decrement ea by one instruction for external
-							* interrupts, so that the instruction will be run */
-
-    asm("SKIP_EA_DEC:");
-    asm("stw r1, 4(sp)"); // save all registers
-    asm("stw r2, 8(sp)");
-    asm("stw r3, 12(sp)");
-    asm("stw r4, 16(sp)");
-    asm("stw r5, 20(sp)");
-    asm("stw r6, 24(sp)");
-    asm("stw r7, 28(sp)");
-    asm("stw r8, 32(sp)");
-    asm("stw r9, 36(sp)");
-    asm("stw r10, 40(sp)");
-    asm("stw r11, 44(sp)");
-    asm("stw r12, 48(sp)");
-    asm("stw r13, 52(sp)");
-    asm("stw r14, 56(sp)");
-    asm("stw r15, 60(sp)");
-    asm("stw r16, 64(sp)");
-    asm("stw r17, 68(sp)");
-    asm("stw r18, 72(sp)");
-    asm("stw r19, 76(sp)");
-    asm("stw r20, 80(sp)");
-    asm("stw r21, 84(sp)");
-    asm("stw r22, 88(sp)");
-    asm("stw r23, 92(sp)");
-    asm("stw r25, 100(sp)"); // r25 = bt (skip r24 = et, because it was saved above)
-    asm("stw r26, 104(sp)"); // r26 = gp
-    asm("stw r27, 108(sp)"); // r27 = sp
-    asm("stw r28, 112(sp)"); // r28 = fp
-    asm("stw r29, 116(sp)"); // r29 = ea
-    asm("stw r30, 120(sp)"); // r30 = ba
-    asm("stw r31, 124(sp)"); // r31 = ra
-    asm("addi fp, sp, 128");
-	
-	// TODO: change all assembly to make sense
-	/* Use embedded assembly language to copy SP in D, then into PDB. */
-    //running_process->user_stack_pointer = (void *) asm("mov sp,d"); // Save the stack pointer to the static_stackframe_pointer
-    register int sp asm("sp");
-    running_process->user_stack_pointer = sp; // Pointer from integer no casting warning
-	
-    /* Second task: switch to kernel stack by modifying stack pointer. */
-    //asm("mov d,sp", kernel_stack_pointer); //assumption dont need to shove sp into register d to read in, can just do below
-	
-	// Type mismatch warning 
-	writeRegisterValueToSP(kernel_stack_pointer);
-	//write in kernel stack pointer address into register sp? could we hard code this into an assembly language file somewhere 274, 371 style?
-	
-}
-
-void QuenosRestoreContext(void) {
-
-    /* Sixth task: switch back to user stack pointer and return */
-	//Write the running->user_stack_pointer sp value we saved when saving context back to register sp
-	writeRegisterValueToSP(running_process->user_stack_pointer);
-
-    // TODO: change all assembly to make sense
-    //kernel_stack_pointer = (void *) asm("mov sp,d"); //get d and put it into sp
-    //asm("mov d,sp", running_process->user_stack_pointer); 
-	
-	// Do we want to keep this or is it just redundant?
-	//register int sp asm("sp");
-    //running_process->user_stack_pointer = sp;
-
-    asm("ldw r1, 4(sp)"); // restore all registers
-    asm("ldw r2, 8(sp)");
-    asm("ldw r3, 12(sp)");
-    asm("ldw r4, 16(sp)");
-    asm("ldw r5, 20(sp)");
-    asm("ldw r6, 24(sp)");
-    asm("ldw r7, 28(sp)");
-    asm("ldw r8, 32(sp)");
-    asm("ldw r9, 36(sp)");
-    asm("ldw r10, 40(sp)");
-    asm("ldw r11, 44(sp)");
-    asm("ldw r12, 48(sp)");
-    asm("ldw r13, 52(sp)");
-    asm("ldw r14, 56(sp)");
-    asm("ldw r15, 60(sp)");
-    asm("ldw r16, 64(sp)");
-    asm("ldw r17, 68(sp)");
-    asm("ldw r18, 72(sp)");
-    asm("ldw r19, 76(sp)");
-    asm("ldw r20, 80(sp)");
-    asm("ldw r21, 84(sp)");
-    asm("ldw r22, 88(sp)");
-    asm("ldw r23, 92(sp)");
-    asm("ldw r24, 96(sp)");
-    asm("ldw r25, 100(sp)"); // r25 = bt
-    asm("ldw r26, 104(sp)"); // r26 = gp
-    // skip r27 because it is sp, and we did not save this on the stack
-    asm("ldw r28, 112(sp)"); // r28 = fp
-    asm("ldw r29, 116(sp)"); // r29 = ea
-    asm("ldw r30, 120(sp)"); // r30 = ba
-    asm("ldw r31, 124(sp)"); // r31 = ra
-    asm("addi sp, sp, 128");
-    asm("eret");
-}
-
 // This is called directly called by the DE2 hardware when a hardware interrupt occurs,
 // It is also called indirectly by the_exception(), the software interrupt
-void    interrupt_handler (void)
+void    interrupt_handler (void) //TODO: if we must move interrupt handler to separate file, then various interactions, how to adapt?
 {
-    /* On entry, all user registers must be saved to the process stack. */
-	QuenosSaveContext();
+    //TODO: SAVE CURRENTLY RUNNING PROCESS STACK POINTER HERE
+        //? switch to kernel stack?
+    // TODO: change all assembly to make sense
+    /* Use embedded assembly language to copy SP in D, then into PDB. */
+    //running_process->user_stack_pointer = (void *) asm("mov sp,d"); // Save the stack pointer to the static_stackframe_pointer
 
+    //register int sp asm("sp");
+    //running_process->user_stack_pointer = sp; // Pointer from integer no casting warning
+
+    //writeRegisterValueToSP(kernel_stack_pointer);
+    //TODO: update to use kernel stack pointer address into register sp? could we hard code
+
+	// ISR
 	register int other_pid asm("r22");
 	register int requestType asm("r23");
 	int ipending;
 
     /* Third task: retrieve arguments for kernel call. */
     /* They are available on the user process stack. */
-	NIOS2_READ_IPENDING(ipending); // Read the interrupt
+	ipending = NIOS2_READ_IPENDING(); // Read the interrupt
 
 	if (ipending) {
 		// This currently has no actions, but this will never be called. It will always go to the else since there are no hardware interrupts yet
@@ -275,13 +179,21 @@ void    interrupt_handler (void)
 			running_process->state = Running;
 		}
 	}
-    /* compiler generates a return-from-interrupt instruction here. */
-	QuenosRestoreContext();
-}
+    // ISR ____________
 
-void the_exception(void) __attribute__ ((section (".exceptions")));
-void the_exception(void) {
-	interrupt_handler();
+    //TODO: we must update running process stack pointer here, with the new thing running
+    /* Sixth task: switch back to user stack pointer and return */
+
+    // Write the running->user_stack_pointer sp value we saved when saving context back to register sp
+
+    //	writeRegisterValueToSP(running_process->user_stack_pointer);
+
+//    kernel_stack_pointer = (void *) asm("mov sp,d"); //get d and put it into sp
+//    asm("mov d,sp", running_process->user_stack_pointer);
+
+    // Do we want to keep this or is it just redundant?
+    //register int sp asm("sp");
+    //running_process->user_stack_pointer = sp;
 }
 
 /* The following function is called _directly_ (i.e., _not_ through an
